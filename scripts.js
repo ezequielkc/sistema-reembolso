59455432db3fd9f0e6c19c7b43de1d8d5459257e
Ezequiel <kcezequiel@gmail.com>
2025-07-30 10:20:55 -0300

    feat: formata vlue para centavos

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
 // Seleciona os elementos do formulário 
 const amount = document.getElementById('amount')
 
+// Captura o eento de input para formatar o valor
 amount.oninput = () => {
+    // Obtém o valor do input e remove caracteres não numéricos
     let value = amount.value.replace(/\D/g, "")
-    
-    amount.value = value
+
+    // Transforma o valor em centavos
+    value = Number(value) / 100
+
+    // Atualiza o valor do input com o valor formatado
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+    // Formata o valor como moeda brasileira
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    // Retorna o valor formatado
+    return value
 }
